7a1c38dc040deb0bb7877262d4a1363bbb70ef2b
tr1g_v0n <8628656+tr1g_v0n@user.noreply.gitee.com>
2023-06-06 16:26:37 +0800

    :sparkles: rustlings

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 [official](https://github.com/rust-lang/rustlings)
 
-[my](https://github.com/tr1gv0n/learn-rust/blob/master/rustlings)
+[my](https://github.com/tr1gv0n/learn-rust/blob/master/rustling)
---
+++ rustling/exercises/modules/modules2.rs
@@ -0,0 +1,29 @@
+// modules2.rs
+// You can bring module paths into scopes and provide new names for them with the
+// 'use' and 'as' keywords. Fix these 'use' statements to make the code compile.
+// Execute `rustlings hint modules2` or use the `hint` watch subcommand for a hint.
+
+
+pub mod delicious_snacks {
+    // TODO: Fix these use statements
+   pub use self::fruits::PEAR as pear;
+   pub use self::veggies::CUCUMBER as cucmber;
+
+    mod fruits {
+        pub const PEAR: &'static str = "Pear";
+        pub const APPLE: &'static str = "Apple";
+    }
+
+     mod veggies {
+        pub const CUCUMBER: &'static str = "Cucumber";
+        pub const CARROT: &'static str = "Carrot";
+    }
+}
+
+fn main() {
+    println!(
+        "favorite snacks: {} and {}",
+        delicious_snacks::pear,
+        delicious_snacks::cucmber
+    );
+}
