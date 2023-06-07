ed93faeb69d4b1da16acc36d655b666c24669362
tr1g_v0n <8628656+tr1g_v0n@user.noreply.gitee.com>
2023-06-07 13:33:36 +0800

    :sparkles: quiz 17 & 30

---
+++ rust-quiz/quiz17/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut a = 5;
+    let mut b = 3;
+    println!("{}", a - ----b);
+    // no unary increment or decrement operator in Rust.
+    // println!("{}", a - (-(-(-)-b))));
+}
