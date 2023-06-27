f6b94a969afa83924ab7c1f42f1969c307c1ccf1
tr1gv0n <920928449@qq.com>
2023-06-27 09:47:44 +0800

    :sparkles: quiz15

---
+++ rust-quiz/quiz15/src/main.rs
@@ -0,0 +1,21 @@
+
+trait Trait {
+    fn f(&self);
+}
+
+impl Trait for u32{
+    fn f(&self){
+        println!("1");
+    }
+}
+
+impl<'a> Trait for &'a i32  {
+    fn f(&self) {
+        print!("2");
+    }
+}
+
+fn main() {
+    let x = &0;
+    x.f();
+}
